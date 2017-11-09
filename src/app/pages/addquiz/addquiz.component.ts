91465611f840475e081f1b1bcb16cd436d24e3d8
Dolan <matt.dolan@capitalone.com>
2017-11-09 14:59:23 -0500

    some add quiz logic

--- src/app/pages/addquiz/addquiz.component.ts
+++ src/app/pages/addquiz/addquiz.component.ts
@@ -1,23 +1,58 @@
 
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 
 @Component({
   selector: 'ngx-add-quiz',
   templateUrl: './addquiz.component.html',
 })
-export class AddQuizComponent {
-  constructor() {}
 
-mycollapse(q){ 
+export class AddQuizComponent implements OnInit {
+  
+  public quizName;
+  public q1;
+  public q1a1;
+  public q1a2;
+  public q1a3;
+  public q1a4;
 
-    const myvar = document.getElementById(q).hidden;
-    if (myvar === false) {
-      document.getElementById(q).hidden = true;
-    } else {
-      document.getElementById(q).hidden = false;
-    }
-  }
+  public radioQ1;
+
+  public headers;
+
+  postData: string;
+  
+  public ngOnInit() {}
+
+  
+  constructor(private http: Http) {}
+
+          mycollapse(q){ 
+
+              const myvar = document.getElementById(q).hidden;
+              if (myvar === false) {
+                document.getElementById(q).hidden = true;
+              } else {
+                document.getElementById(q).hidden = false;
+              }
+            }
+
+            public addQuizSubmit(){
+              var x = this.radioQ1;
+alert(x);
+              this.headers = new Headers();
+              this.headers.append('Content-Type', 'application/x-www-form-urlencoded');
+              this.http.post('http://localhost:8080/test-your-knowledge/add'
+              , `quizName=${this.quizName}&q1=${this.q1}&q1a1=${this.q1a1}&q1a2=${this.q1a2}&q1a3=${this.q1a3}&q1a4=${this.q1a4}&radioQ1=${this.radioQ1}`, {headers: this.headers})
+              .subscribe(
+                  data => console.log(data),
+                    
+                      
+              );
+              // alert(this.postData);
+
+            }
+
 }
 
 
