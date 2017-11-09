91fc274088bc51ff3cee76cc617d6a1852fbc396
Rajesh <rajesh.chintkuntlawar@captialone.com>
2017-11-09 16:22:26 -0500

    Added navigation from login to dashboard

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,38 +1,40 @@
-
 import { Component, OnInit } from '@angular/core';
-import {Http, Headers} from '@angular/http';
-
+import { Http, Headers } from '@angular/http';
+import { Router } from '@angular/router';
+import {PagesRoutingModule} from '../pages-routing.module';
 
 @Component({
-  selector: 'ngx-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+    selector: 'ngx-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
     public username;
     public password;
     public headers;
-    
-    postData: string;
-    userrole: string;
-    
+    public userData;
+
     public ngOnInit() {
     }
-    
-    constructor(private http: Http) {
+
+    constructor(private router :Router, private http: Http) {
     }
-    
+
     public loginSubmit() {
-     this.headers = new Headers();
-     this.headers.append('Content-Type', 'application/x-www-form-urlencoded');
-     this.http.post('http://localhost:8080/test-your-knowledge/login'
-      , `username=${this.username}&password=${this.password}`, {headers: this.headers})
-      .subscribe(
-          data => this.postData = JSON.stringify(data),
-           
-             
-      );
-      // alert(this.postData);
+        this.headers = new Headers();
+        this.headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        this.http.post('http://localhost:8080/test-your-knowledge/login'
+            , `username=${this.username}&password=${this.password}`, { headers: this.headers })
+            .subscribe(
+            (data) => {
+            this.userData = data.json();
+            // console.log(this.userData.userrole);
+                if (this.userData.userrole === "sme") {
+                        this.router.navigate(['pages/dashboard']);
+                }
+            }
+
+            );
     }
 
 }
