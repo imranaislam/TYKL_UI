a2fe485f0b50bfbb3ab4ef58bad367b91eee9c7f
Brunda Prakash <brunda.prakash@capitalone.com>
2017-11-10 16:56:56 -0500

    updated registration changes

--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Router } from '@angular/router';
+import { PagesRoutingModule } from '../pages-routing.module';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component ({
     selector: 'ngx-registration',
     templateUrl: './registration.component.html',
-    styleUrls: ['registration.component.css'],
+    styleUrls: ['./registration.component.css'],
 })
-export class RegistrationComponent {
-    constructor() {}
+export class RegistrationComponent implements OnInit { 
+
+    public input: any;
+
+    public ngOnInit(){
     }
+
+    public constructor(private router: Router, private http: Http) {
+        this.input = {
+            "firstName": "",
+            "lastName": "",
+            "userName": "",
+            "password": "",
+            "email": "",
+            "roleId": ""
+        };
+    }
+
+    public registrationSubmit(){
+        //console.log("Enter registrationSubmit");
+        if ( this.input.firstName && 
+                this.input.lastName && 
+                this.input.userName && 
+                this.input.roleId && 
+                this.input.email && 
+                this.input.password){
+            let headers = new Headers({ "content-type": "application/json" });
+            let options = new RequestOptions({ headers: headers});
+            this.http.post("http://localhost:8080/test-your-knowledge/insertUserOrAdmin", 
+                JSON.stringify(this.input), options)
+            .subscribe(result => {
+                console.log(result);
+                this.router.navigate(['/auth/login']);
+            });
+        }
+        
+
+
+    }
+ 
+}
+
