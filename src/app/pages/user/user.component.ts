eaf35b83a2c558610770a5da11df11980af8cd92
Rajesh <rajesh.chintkuntlawar@captialone.com>
2017-11-11 15:44:19 -0500

    resolved lint errors

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -26,16 +26,13 @@ export class UserComponent implements OnInit {
         this.headers.append('Content-Type', 'application/x-www-form-urlencoded');
         this.http.post('http://localhost:8080/test-your-knowledge/takequiz'
             , `subjectArea=${this.subjectArea}&expertiseLevel=${this.expertiseLevel}`, { headers: this.headers })
-        .subscribe(
+            .subscribe(
             (questions) => {
                 if (questions.status === 200) {
                     this.router.navigate(['pages/takequiz']);
-                    console.log(questions);
                 }
             },
             (error) => {
-                console.log(error.status);
-                
                 if (error.status === 400) {
                     this.router.navigate(['pages/takequiz']);
                     this.message = 'Our Sincere Apologies.  We are working on creating challenges in the subject area you chose.  Please come back soon and try again.';
