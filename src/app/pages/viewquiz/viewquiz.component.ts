6a4a54e433109068c809d7e058819ec9a27245e9
Islam, Imrana <imrana.islam@capitalone.com>
2017-11-14 18:16:15 -0500

    Added code for delete question

--- src/app/pages/viewquiz/viewquiz.component.ts
+++ src/app/pages/viewquiz/viewquiz.component.ts
@@ -8,23 +8,38 @@ import { PagesRoutingModule } from '../pages-routing.module';
   templateUrl: './viewquiz.component.html',
   styleUrls: ['viewquiz.component.css'],
 })
-export class ViewQuizComponent implements OnInit {
-  quizList: any;
+export class ViewQuizComponent implements OnInit { 
+  quizList: any ;
   message: String;
 
-  constructor(private router: Router, private http: Http) { }
+  constructor(private router: Router, private http: Http) {}
 
   public ngOnInit() {
+    this.retrieveQuiz();
+
+
+  }
+retrieveQuiz(){
     this.http.get('http://localhost:8080/test-your-knowledge/viewquiz').subscribe((resp) => {
-      if (resp.status === 200) {
-        this.quizList = resp.json();
-      }
-    },
-      (error) => {
-        if (error.status === 400) {
+      if (resp.status === 200){
+      this.quizList = resp.json();
+      // console.log(this.quizList);
+    }
+  },
+  (error) => {
+      if (error.status === 400) {
           this.message = 'Our Application experienced an issue.  Please try again.';
-        }
-      },
+      }
+  },
     );
   }
+
+  retrieveQuestions(quizId){
+
+   localStorage.setItem('quiz_id', quizId);
+   // alert(localStorage.getItem('quiz_id'));
+   this.router.navigate(['pages/updatequiz']);
+   
+  }
+
 }
