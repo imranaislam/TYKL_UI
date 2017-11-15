5330ff381de067364113f34cce1e6fd530c30aaf
Dolan <matt.dolan@capitalone.com>
2017-11-15 14:13:54 -0500

    Update to Home Page

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
+
+  usertype: boolean;
+  userString: String;
+  firstName: String;
+  
+  public ngOnInit() {
+
+    this.userString = localStorage.getItem ('theUser'); 
+    if (this.userString === 'sme') {
+      this.usertype = true;
+    } else {
+      this.usertype = false;
+    }
+    
+    this.firstName = localStorage.getItem('firstName');
+}
 }
